5c58cf262400f3bc1ff17116decd066191c997ff
Mika Landelius <mila6004@pelle2.uppmax.uu.se>
2026-04-16 13:48:59 +0200

    update bwa script

--- scripts/3_assembly_polishing/bwa/bwa.sh
+++ scripts/3_assembly_polishing/bwa/bwa.sh
@@ -11,9 +11,9 @@
 module load bwa-mem2/2.3-GCC-13.3.0
 module load SAMtools/1.22.1-GCC-13.3.0
 
-bwa-mem2 index /home/mila6004/Genomeanalysis/results/flye/flye.fasta
+bwa-mem2 index /home/mila6004/Genomeanalysis/results/flye/assembly.fasta
 
-bwa-mem2 mem -t 2 /home/mila6004/Genomeanalysis/results/flye/flye.fasta /home/mila6004/Genomeanalysis/data/Illumina_reads/chr3_illumina_R1.fastq.gz /home/mila6004/Genomeanalysis/data/Illumina_reads/chr3_illumina_R2.fastq.gz | samtools view -b -o bwa.bam
+bwa-mem2 mem -t 2 /home/mila6004/Genomeanalysis/results/flye/assembly.fasta /home/mila6004/Genomeanalysis/data/Illumina_reads/chr3_illumina_R1.fastq.gz /home/mila6004/Genomeanalysis/data/Illumina_reads/chr3_illumina_R2.fastq.gz | samtools view -b -o bwa.bam
 
 samtools sort -@ 2 -o bwa.sorted.bam bwa.bam
 
