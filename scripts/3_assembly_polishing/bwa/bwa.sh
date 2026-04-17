e2c44c7ad74be63a8a9b152979d04c3e9dca556c
Mika Landelius <mila6004@pelle2.uppmax.uu.se>
2026-04-17 15:13:08 +0200

    finish run bwa

--- scripts/3_assembly_polishing/bwa/bwa.sh
+++ scripts/3_assembly_polishing/bwa/bwa.sh
@@ -2,7 +2,7 @@
 #SBATCH -A uppmax2026-1-61
 #SBATCH -p pelle
 #SBATCH -c 2
-#SBATCH -t 02:00:00
+#SBATCH -t 20:00:00
 #SBATCH --mem=32G
 #SBATCH -J bwa
 #SBATCH -o bwa.out
@@ -11,9 +11,9 @@
 module load bwa-mem2/2.3-GCC-13.3.0
 module load SAMtools/1.22.1-GCC-13.3.0
 
-bwa-mem2 index /home/mila6004/Genomeanalysis/results/flye/assembly.fasta
+bwa-mem2 index /home/mila6004/Genomeanalysis/results/2_genome_assembly/flye/assembly.fasta
 
-bwa-mem2 mem -t 2 /home/mila6004/Genomeanalysis/results/flye/assembly.fasta /home/mila6004/Genomeanalysis/data/Illumina_reads/chr3_illumina_R1.fastq.gz /home/mila6004/Genomeanalysis/data/Illumina_reads/chr3_illumina_R2.fastq.gz | samtools view -b -o bwa.bam
+bwa-mem2 mem -t 2 /home/mila6004/Genomeanalysis/results/2_genome_assembly/flye/assembly.fasta /home/mila6004/Genomeanalysis/data/Illumina_short_reads/chr3_illumina_R1.fastq.gz /home/mila6004/Genomeanalysis/data/Illumina_short_reads/chr3_illumina_R2.fastq.gz | samtools view -b -o bwa.bam
 
 samtools sort -@ 2 -o bwa.sorted.bam bwa.bam
 
