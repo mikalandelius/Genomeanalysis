ab305e5b7e631254fc78b895d780781eb03dd909
Mika Landelius <mila6004@pelle2.uppmax.uu.se>
2026-04-28 14:18:42 +0200

    finish run merqury

--- scripts/4_quality_evaluation/merqury/merqury_run1.sh
+++ scripts/4_quality_evaluation/merqury/merqury_run1.sh
@@ -5,11 +5,11 @@
 #SBATCH -t 10:00:00
 #SBATCH --mem=32G
 #SBATCH -J merqury
-#SBATCH -o merqury.out
-#SBATCH -e merqury.err
+#SBATCH -o merqury_run1.out
+#SBATCH -e merqury_run1.err
 
 module load merqury/20240628-1ad7c32-gfbf-2024a
 
 merqury.sh reads.meryl \
 /home/mila6004/Genomeanalysis/results/3_assembly_polishing/pilon/pilon.fasta \
-/home/mila6004/Genomeanalysis/results/4_quality_evaluation/merqury/run1
+run1
