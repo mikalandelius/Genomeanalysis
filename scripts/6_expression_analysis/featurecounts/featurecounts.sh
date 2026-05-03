2f0f73cbd3d27b15824f077f95beb0ad99f52421
Mika Landelius <mila6004@pelle1.uppmax.uu.se>
2026-05-03 16:18:13 +0200

    update featurecounts script

---
+++ scripts/6_expression_analysis/featurecounts/featurecounts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 08:00:00
+#SBATCH --mem=32G
+#SBATCH -J featurecounts
+#SBATCH -o featurecounts.out
+#SBATCH -e featurecounts.err
+
+module load Subread/2.1.1-GCC-13.3.0
+
+featureCounts \
+-T 2 \
+-p \
+--countReadPairs \
+-s 1 \
+-a /home/mila6004/Genomeanalysis/results/5_annotation/braker3/braker.gtf \
+-o featurecounts.txt \
+/home/mila6004/Genomeanalysis/results/6_expression_analysis/star/Control_1Aligned.sortedByCoord.out.bam \
+/home/mila6004/Genomeanalysis/results/6_expression_analysis/star/Heat_1Aligned.sortedByCoord.out.bam
